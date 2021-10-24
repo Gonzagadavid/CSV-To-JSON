fa0f90382599d8f67fab51fbda18ca5b0cf28f68
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-23 23:35:45 -0300

    refatorado conversão de json para csv, adicionando a garantia de que dados de numeros decimais utilize ponto ao invés de vírguala

--- js/functions/toCsv.js
+++ js/functions/toCsv.js
@@ -2,13 +2,17 @@ import objectFile from '../index.js';
 import erroMessage from '../views/erroMessage.js';
 import checkJson from './checkJson.js';
 
+function objectToString (array) {
+  return array.map((str) => str.toString().replace(/\,/g, '.')).join(',')
+}
+
 export default function toCsv() {
   const textJson = document.getElementById('text-initial').value;
   if (!checkJson(textJson)) return erroMessage('O arquivo inserido não está no formato JSON');
   let arrObjects = JSON.parse(textJson);
   if (!Array.isArray(arrObjects)) arrObjects = [arrObjects];
   const keys = `${Object.keys(arrObjects[0]).join(',')}\n`;
-  const values = arrObjects.map((object) => Object.values(object).join(',')).join('\n');
+  const values = arrObjects.map((object) => objectToString(Object.values(object))).join('\n');
   const csvText = `${keys}${values}`;
   const convertedText = document.getElementById('converted-text');
   convertedText.value = csvText;
